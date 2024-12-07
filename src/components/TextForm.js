5d95eedcbab19e1533d6405ead94ef5a88b712c6
Sushil Kumar Thakur <sushi@SUSHILTHAKUR>
2024-12-07 21:56:16 +0545

    push

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -43,7 +43,19 @@ const handleinverseclick = () => {
 <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear the text</button>
 <button className="btn btn-primary mx-2" onClick={handleinverseclick}>invert</button>
 
-
+<p class="d-inline-flex gap-1">
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Link with href
+  </a>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Button with data-bs-target
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+  </div>
+</div>
 
 
 <div className="container my-3 ">
